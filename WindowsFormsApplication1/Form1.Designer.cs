6740d5fd7c0c899ee56f0cfc5de53839dbb7aaa4
Chris <Chris@LENOVO-PC>
2018-02-27 04:26:36 +0200

    First Commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.textBoxBarcode = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblBarcode = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.imgFree = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnPrint.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnPrint.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrint.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btnPrint.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPrint.Location = new System.Drawing.Point(246, 110);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(141, 42);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "Εκτύπωση";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            this.btnPrint.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxBarcode
+            // 
+            this.textBoxBarcode.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBoxBarcode.Location = new System.Drawing.Point(12, 47);
+            this.textBoxBarcode.Name = "textBoxBarcode";
+            this.textBoxBarcode.Size = new System.Drawing.Size(158, 27);
+            this.textBoxBarcode.TabIndex = 1;
+            this.textBoxBarcode.TextChanged += new System.EventHandler(this.textBoxBarcode_TextChanged);
+            this.textBoxBarcode.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxBarcode_KeyDown);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(271, 160);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(89, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Νέο Barcode";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // lblBarcode
+            // 
+            this.lblBarcode.AutoSize = true;
+            this.lblBarcode.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblBarcode.Location = new System.Drawing.Point(7, 19);
+            this.lblBarcode.Name = "lblBarcode";
+            this.lblBarcode.Size = new System.Drawing.Size(98, 25);
+            this.lblBarcode.TabIndex = 3;
+            this.lblBarcode.Text = "Barcode";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblPrice.Location = new System.Drawing.Point(7, 89);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(52, 25);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "Τιμή";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBoxPrice.Location = new System.Drawing.Point(12, 117);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(101, 27);
+            this.textBoxPrice.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(116, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "€";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(7, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ποσότητα";
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.numericUpDown.Location = new System.Drawing.Point(13, 189);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(100, 27);
+            this.numericUpDown.TabIndex = 8;
+            this.numericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Hobo Std", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.label3.Location = new System.Drawing.Point(326, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Made by Xristaras";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblName.ForeColor = System.Drawing.Color.DimGray;
+            this.lblName.Location = new System.Drawing.Point(195, 47);
+            this.lblName.MaximumSize = new System.Drawing.Size(240, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblName.Size = new System.Drawing.Size(0, 18);
+            this.lblName.TabIndex = 10;
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgFree
+            // 
+            this.imgFree.AutoSize = true;
+            this.imgFree.Image = global::WindowsFormsApplication1.Properties.Resources.checked__1_;
+            this.imgFree.Location = new System.Drawing.Point(104, 19);
+            this.imgFree.MinimumSize = new System.Drawing.Size(25, 25);
+            this.imgFree.Name = "imgFree";
+            this.imgFree.Padding = new System.Windows.Forms.Padding(5);
+            this.imgFree.Size = new System.Drawing.Size(25, 25);
+            this.imgFree.TabIndex = 11;
+            this.imgFree.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 243);
+            this.Controls.Add(this.imgFree);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblBarcode);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.textBoxBarcode);
+            this.Controls.Add(this.btnPrint);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Label For Store";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.TextBox textBoxBarcode;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblBarcode;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label imgFree;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Xml.Xsl;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            if ((textBoxBarcode.TextLength > 0) && (textBoxPrice.TextLength > 0))
+            {
+              
+                string[] lines = {
+                    "^XSET,CODEPAGE,15",
+                    "^Q25,3",
+                    "^W40",
+                    "^H8",
+                    "^P"+numericUpDown.Value.ToString(),
+                    "^S3",
+                    "^AD",
+                    "^C1",
+                    "^R8",
+                    "~Q-8",
+                    "^O0",
+                    "^D0",
+                    "^E12",
+                    "~R255",
+                    "^XSET,ROTATION,0",
+                    "^L",
+                    "Dy2-me-dd",
+                    "Th:m:s",
+                    "Y223,17,RoundRect1-71",
+                    "AE,241,155,1,1,0,3,"+textBoxPrice.Text.ToString()+ "€",
+                    "BQ,66,187,3,5,80,3,3,"+textBoxBarcode.Text.ToString(),
+                    "AB,185,146,1,1,0,3,25.36.14",
+                    "E"
+                };
+                // WriteAllLines creates a file, writes a collection of strings to the file,
+                // and then closes the file.  You do NOT need to call Flush() or Close().
+
+                try
+                {
+                    System.IO.File.WriteAllLines(@"PrintFiles\\demo.zpl", lines ,Encoding.GetEncoding(1250) );
+                    while (true)
+                    {
+                        if (File.Exists(@"PrintFiles\\demo.zpl"))
+                        {
+                            System.Diagnostics.Process.Start(@"PrintFiles\\demo.zpl");
+                            //den anagorizei to path kai o ektipotis vgazei "file Name not found"
+                            break;
+                        }
+                    }
+                }catch(Exception exe)
+                {
+
+                }
+
+                
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Τα πεδία barcode και τιμή πρέπει να ναι συμπληρωμένα!! ", "Βιάστηκες!",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+
+        public void searchDb()
+        {
+            SqlConnection con = new SqlConnection("Data Source=LENOVO-PC\\INFLOWSQL;Initial Catalog=inFlow;Integrated Security=True;");
+            SqlDataAdapter sda = new SqlDataAdapter("select UnitPrice,Name from dbo.BASE_ItemPrice,dbo.BASE_Product where BarCode = '" + textBoxBarcode.Text + "' and dbo.BASE_ItemPrice.ProdId=dbo.BASE_Product.ProdId;", con);
+            DataTable dt = new DataTable();
+
+            sda.Fill(dt);
+
+            if (dt.Rows.Count == 0)
+            {
+                imgFree.Visible = true;
+            }
+            else
+            {
+                string price = dt.Rows[0][0].ToString();
+                textBoxPrice.Text = price.Substring(0, price.Length - 3);
+                lblName.Text = dt.Rows[0][1].ToString();
+            }
+        }
+
+        private void textBoxBarcode_TextChanged(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+
+        private void clearAll()
+        {
+            textBoxPrice.Text = "";
+            lblName.Text = "";
+            imgFree.Visible = false;
+        }
+
+
+        private void textBoxBarcode_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                searchDb();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+
+
+      
+    }
+}
